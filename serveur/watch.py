20b891002e2bc4091aaa7cc190240a25cb6bdca3
YohanDerenne <yohan.derenne@gmail.com>
2024-05-03 15:15:46 +0200

    [watch] refactor

--- serveur/watch.py
+++ serveur/watch.py
@@ -1,19 +1,15 @@
 import argparse
 import os
-import json
+import conf
 
-configPath = os.path.dirname(__file__) + '/../config/symfony-conf.json'
-with open(configPath, 'r') as f:
-    config = json.load(f)
-projects = config
+projects = conf.load('symfony-conf.json')
 
 cmd = 'npm run watch'
 
 def main():   
     parser = argparse.ArgumentParser(
         description='Run \"' + cmd + '\" on a project'
-    )
-    
+    )    
     parser.add_argument(
         'project',
         choices=projects.keys(),
@@ -22,15 +18,12 @@ def main():
     )
     args = parser.parse_args()
 
-    if args.project in projects :
-        print("===============================")
-        print(args.project)
-        print(projects[args.project])
-        print("===============================")
-        os.chdir(projects[args.project])
-        os.system(cmd)
-    else :
-        print("Project \"" + args.project + "\" is unknow")
+    print("===============================")
+    print(args.project)
+    print(projects[args.project])
+    print("===============================")
+    os.chdir(projects[args.project])
+    os.system(cmd)
 
 if __name__ == "__main__":
     main()
