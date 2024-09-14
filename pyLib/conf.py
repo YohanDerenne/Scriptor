e181ad4f2ebdf2bd1db48514d564f1e352415c3b
YohanDerenne <yohan.derenne@gmail.com>
2024-09-14 13:22:40 +0200

    [Feature] Conf : Help with template dir if no conf

--- pyLib/conf.py
+++ pyLib/conf.py
@@ -3,6 +3,7 @@ import os
 import log
 
 confFolder = os.path.dirname(__file__) + '/../config/'
+confTemplateFolder = os.path.dirname(__file__) + '/../config-template/'
 
 def load(jsonFileName):
     configPath = confFolder + jsonFileName
@@ -12,5 +13,6 @@ def load(jsonFileName):
             return json.load(f)
     except FileNotFoundError:
         log.error('Le fichier de configuration \"' + jsonFileName + 
-                  "\" est introuvable dans le dossier " + os.path.realpath(confFolder))
+                  "\" est introuvable dans le dossier " + os.path.realpath(confFolder) + 
+                  " (voir exemple dans " + os.path.realpath(confTemplateFolder) + ")")
         exit()
