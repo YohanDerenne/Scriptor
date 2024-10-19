a372a3e306d2af62824dfab1eaff5ce4de031392
YohanDerenne <yohan.derenne@gmail.com>
2024-10-19 09:13:28 +0200

    [Feature/WIP] mgit : branch diff

--- util/mgit.py
+++ util/mgit.py
@@ -29,13 +29,16 @@ def main():
     if args.fetch :
         print("fetching...")
         for dir in config[args.project] :
-            result = terminal.cmdOutput("git -C " + dir + " fetch")
+            print(dir)
+            result = terminal.cmdOutput("git -C " + dir + " fetch --all --quiet")
             error = terminal.getError(result)
             if error != "":
                 log.error(terminal.getError(result))
-
-    # if exitCode > 0:
-    #     log.error("La cmd a échoué")
+            else :
+                hashLocal = terminal.cmdOutput("git -C " + dir + " rev-parse @").stdout
+                # hashRemote = terminal.cmdOutput("git -C " + dir + " rev-parse @{u}").stdout
+                # hashBase = terminal.cmdOutput("git -C " + dir + " merge-base @ @{u}").stdout
+                print(hashLocal)
     
 
 if __name__ == '__main__':
