a0e88283e87616d84b5c73f4455fcf1f4c6a04ee
YohanDerenne <yohan.derenne@gmail.com>
2025-02-18 22:59:43 +0100

    [Feature] gitalias : Add git alias in global config

---
+++ script/util/gitalias.py
@@ -0,0 +1,43 @@
+import configparser
+import os
+
+import log
+
+gitConfigGlobalPath = os.path.expanduser("~") + '/.gitconfig'
+gitConfigScriptorPath = os.path.dirname(__file__) + '/../../resources/.gitconfig'
+
+def main():
+    # Check files
+    if not os.path.exists(gitConfigGlobalPath):
+        log.error(f"Global gitconfig file '{gitConfigGlobalPath}' not found.")
+        return
+    
+    if not os.path.exists(gitConfigScriptorPath):
+        log.error(f"Scriptor gitconfile file resource '{gitConfigScriptorPath}' not found.")
+        return
+    
+    gitConfigGlobalParser = configparser.ConfigParser()
+    gitConfigGlobalParser.read(gitConfigGlobalPath)
+    
+    gitConfigScriptorParser = configparser.ConfigParser()
+    gitConfigScriptorParser.read(gitConfigScriptorPath)
+
+    # Merge and skip duplicate
+    for section in gitConfigScriptorParser.sections():
+        if not gitConfigGlobalParser.has_section(section):
+            gitConfigGlobalParser.add_section(section)
+        
+        for key, value in gitConfigScriptorParser.items(section):
+            if not gitConfigGlobalParser.has_option(section, key) or gitConfigGlobalParser.get(section, key) != value :
+                gitConfigGlobalParser.set(section, key, value)
+            else:
+                log.info(f"Duplicate found inside section '{section}', key '{key}' : ingore it.")
+
+    # Save file global
+    with open(gitConfigGlobalPath, 'w') as configfile:
+        gitConfigGlobalParser.write(configfile)
+
+    log.sucess(f"Merge done in '{gitConfigGlobalPath}'.")
+
+if __name__ == '__main__':
+    main()
